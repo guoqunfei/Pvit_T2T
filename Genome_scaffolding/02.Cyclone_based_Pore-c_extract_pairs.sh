0e054d6b1891d7a631c32a7440dbf965964d7083
wanyi0309 <1476347384@qq.com>
2024-09-02 21:23:52 +0800

    Update 02.Cyclone_based_Pore-c_extract_pairs.sh

--- Genome_scaffolding/02.Cyclone_based_Pore-c_extract_pairs.sh
+++ Genome_scaffolding/02.Cyclone_based_Pore-c_extract_pairs.sh
@@ -3,7 +3,7 @@
 #### Adjacent fragment pairs were extracted from wf-pore-c output file <null.ns.bam>
 #### Wf-pore-c output file <null.ns.bam> is in the output/bams directory of wf-pore-c output results
 
-SAMTOOLS="path/to/your/samtools"
+SAMTOOLS="/path/to/your/samtools"
 EXTRACT_SCRIPT="02.Cyclone_based_Pore-c_extract_pairs.py"
 INPUT_BAM="output/bams/null.ns.bam"
 FILTERED_BAM="null.ns.filtered.bam"
