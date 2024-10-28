707d9837620aec9f9dc5486f187e56a9579dea0e
guoqunfei <50896478+guoqunfei@users.noreply.github.com>
2024-10-28 11:38:21 +0800

    Add files via upload

---
+++ 01.Genome_assembly/02.Cyclone_contig_assembly/NextDenovo.sh
@@ -0,0 +1,7 @@
+echo start at `date`
+
+## NextDenovo software can be installed from https://github.com/Nextomics/NextDenovo
+
+path/to/NextDenovo_2.5.0/NextDenovo/nextDenovo run.cfg
+
+echo end at `date`
